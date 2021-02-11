85a0b9480d7655834d35b67cb8049b07ea8deed5
Brian Anglin <brian@sharethat.to>
2021-02-10 20:43:52 -0700

    Last check in before pods

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "ShareThatTo",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -15,14 +19,16 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/venmo/DVR", from: "2.0.0"),
-           .package(url: "https://github.com/facebook/facebook-ios-sdk", from: "9.0.0")
+//        .package(url: "https://github.com/facebook/facebook-ios-sdk", from: "8.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ShareThatTo",
-            dependencies: [],
+            dependencies: [
+//                .product(name: "FacebookShare", package: "Facebook")
+            ],
             resources: [
                 .copy("Assets")
             ]),
---
+++ Sources/ShareThatTo/ShareSheet/Outlets/FacebookOutlet.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Brian Anglin on 2/10/21.
+//
+
+
+import Foundation
+
